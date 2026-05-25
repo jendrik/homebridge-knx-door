bccf7671aa0f37e87229ca05b8b803124a4415e0
Jendrik <jendrik@bertram.eu>
2026-05-25 13:00:53 +0200

    refactor: modernize static platform internals

--- src/accessory.ts
+++ src/accessory.ts
@@ -1,186 +1,78 @@
-import type { AccessoryConfig, AccessoryPlugin, Service } from 'homebridge';
+import type { AccessoryPlugin, Characteristic, CharacteristicValue, Service, WithUUID } from 'homebridge';
 
 import { Datapoint } from 'knx';
-import { ACCESSORY_UUID_NAMESPACE, PLUGIN_VERSION, PLUGIN_DISPLAY_NAME } from './settings.js';
+
+import { ACCESSORY_UUID_NAMESPACE, PLUGIN_DISPLAY_NAME, PLUGIN_VERSION } from './settings.js';
 import type { ContactSensorPlatform } from './platform.js';
 
+export type ContactSensorDeviceConfig = {
+  name: string;
+  listen: string;
+};
+
+type FakeGatoHistoryEntry = {
+  time?: number;
+  status?: boolean;
+};
+
+type FakeGatoHistoryService = Service & {
+  history: FakeGatoHistoryEntry[];
+  getInitialTime(): number | undefined;
+  _addEntry(entry: Required<FakeGatoHistoryEntry>): void;
+};
+
+type FakeGatoHistoryConstructor = new (
+  accessoryType: 'door',
+  accessory: AccessoryPlugin,
+  options: { storage: 'fs'; log: ContactSensorPlatform['log'] },
+) => FakeGatoHistoryService;
+
+type EveCharacteristicConstructor = WithUUID<new () => Characteristic>;
+
+const EVE_CONTACT_SENSOR_TIMES_OPENED_UUID = 'E863F129-079E-48FF-8F27-9C2605A29F52';
+const EVE_CONTACT_SENSOR_OPEN_DURATION_UUID = 'E863F118-079E-48FF-8F27-9C2605A29F52';
+const EVE_CONTACT_SENSOR_CLOSED_DURATION_UUID = 'E863F119-079E-48FF-8F27-9C2605A29F52';
+const EVE_CONTACT_SENSOR_LAST_ACTIVATION_UUID = 'E863F11A-079E-48FF-8F27-9C2605A29F52';
 
 export class ContactSensorAccessory implements AccessoryPlugin {
-  private readonly uuid_base: string;
+  private readonly uuidBase: string;
   private readonly name: string;
-  private readonly displayName: string;
   private readonly listen: string;
 
   private readonly contactSensorService: Service;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  private readonly loggingService: any;
+  private readonly loggingService: FakeGatoHistoryService;
   private readonly informationService: Service;
 
   constructor(
     private readonly platform: ContactSensorPlatform,
-    private readonly config: AccessoryConfig,
+    private readonly config: ContactSensorDeviceConfig,
   ) {
-
-    class EveContactSensorTimesOpened extends platform.Characteristic {
-      public static readonly UUID: string = 'E863F129-079E-48FF-8F27-9C2605A29F52';
-
-      constructor() {
-        super('Times Opened', EveContactSensorTimesOpened.UUID, {
-          format: platform.api.hap.Formats.UINT32,
-          perms: [platform.api.hap.Perms.PAIRED_READ, platform.api.hap.Perms.NOTIFY],
-        });
-        this.value = this.getDefaultValue();
-      }
-    }
-
-    class EveContactSensorOpenDuration extends platform.Characteristic {
-      public static readonly UUID: string = 'E863F118-079E-48FF-8F27-9C2605A29F52';
-
-      constructor() {
-        super('Open Duration', EveContactSensorOpenDuration.UUID, {
-          format: platform.api.hap.Formats.UINT32,
-          unit: platform.api.hap.Units.SECONDS,
-          perms: [platform.api.hap.Perms.PAIRED_READ, platform.api.hap.Perms.NOTIFY, platform.api.hap.Perms.PAIRED_WRITE],
-        });
-        this.value = this.getDefaultValue();
-      }
-    }
-
-    class EveContactSensorClosedDuration extends platform.Characteristic {
-      public static readonly UUID: string = 'E863F119-079E-48FF-8F27-9C2605A29F52';
-
-      constructor() {
-        super('Closed Duration', EveContactSensorClosedDuration.UUID, {
-          format: platform.api.hap.Formats.UINT32,
-          unit: platform.api.hap.Units.SECONDS,
-          perms: [platform.api.hap.Perms.PAIRED_READ, platform.api.hap.Perms.NOTIFY, platform.api.hap.Perms.PAIRED_WRITE],
-        });
-        this.value = this.getDefaultValue();
-      }
-    }
-
-    class EveContactSensorLastActivation extends platform.Characteristic {
-      public static readonly UUID: string = 'E863F11A-079E-48FF-8F27-9C2605A29F52';
-
-      constructor() {
-        super('Last Activation', EveContactSensorLastActivation.UUID, {
-          format: platform.api.hap.Formats.UINT32,
-          unit: platform.api.hap.Units.SECONDS,
-          perms: [platform.api.hap.Perms.PAIRED_READ, platform.api.hap.Perms.NOTIFY],
-        });
-        this.value = this.getDefaultValue();
-      }
-    }
-
     this.name = config.name;
     this.listen = config.listen;
-    this.uuid_base = platform.uuid.generate(ACCESSORY_UUID_NAMESPACE + '-' + this.name + '-' + this.listen);
-    this.displayName = this.uuid_base;
+    this.uuidBase = platform.uuid.generate(`${ACCESSORY_UUID_NAMESPACE}-${this.name}-${this.listen}`);
 
     this.informationService = new platform.Service.AccessoryInformation()
       .setCharacteristic(platform.Characteristic.Name, this.name)
-      .setCharacteristic(platform.Characteristic.Identify, this.name)
       .setCharacteristic(platform.Characteristic.Manufacturer, '@jendrik')
       .setCharacteristic(platform.Characteristic.Model, PLUGIN_DISPLAY_NAME)
-      .setCharacteristic(platform.Characteristic.SerialNumber, this.displayName)
+      .setCharacteristic(platform.Characteristic.SerialNumber, this.uuidBase)
       .setCharacteristic(platform.Characteristic.FirmwareRevision, PLUGIN_VERSION);
 
     this.contactSensorService = new platform.Service.ContactSensor(this.name);
     this.contactSensorService.getCharacteristic(platform.Characteristic.StatusActive).updateValue(true);
+    this.addEveCharacteristics();
 
-    // times opened
-    this.contactSensorService.addCharacteristic(EveContactSensorTimesOpened);
-    this.contactSensorService.getCharacteristic(EveContactSensorTimesOpened).onGet(async () => {
-      let count = 0;
-      let prevStatus = undefined;
-      for (let i = 0; i < this.loggingService.history.length; ++i) {
-        const status = this.loggingService.history[i].status;
-        if (status === undefined) {
-          continue;
-        }
-        if (status !== prevStatus && prevStatus !== true) {
-          count++;
-        }
-        prevStatus = status;
-      }
-      return count;
-    });
+    const FakeGatoHistory = platform.fakeGatoHistoryService as FakeGatoHistoryConstructor;
+    this.loggingService = new FakeGatoHistory('door', this, { storage: 'fs', log: platform.log });
 
-    // open duration
-    this.contactSensorService.addCharacteristic(EveContactSensorOpenDuration);
-    this.contactSensorService.getCharacteristic(EveContactSensorOpenDuration).onGet(async () => {
-      let duration = 0;
-      let prevStatus = undefined;
-      let prevTime = undefined;
-      for (let i = 0; i < this.loggingService.history.length; ++i) {
-        const status = this.loggingService.history[i].status;
-        const time = this.loggingService.history[i].time;
-        if (status === undefined || time === undefined) {
-          continue;
-        }
-        if (prevStatus === true && prevTime !== undefined) {
-          duration += time - prevTime;
-        }
-        prevStatus = status;
-        prevTime = time;
-      }
-
-      return duration;
-    });
-
-    // closed duration
-    this.contactSensorService.addCharacteristic(EveContactSensorClosedDuration);
-    this.contactSensorService.getCharacteristic(EveContactSensorClosedDuration).onGet(async () => {
-      let duration = 0;
-      let prevStatus = undefined;
-      let prevTime = undefined;
-      for (let i = 0; i < this.loggingService.history.length; ++i) {
-        const status = this.loggingService.history[i].status;
-        const time = this.loggingService.history[i].time;
-        if (status === undefined || time === undefined) {
-          continue;
-        }
-        if (prevStatus === false && prevTime !== undefined) {
-          duration += time - prevTime;
-        }
-        prevStatus = status;
-        prevTime = time;
-      }
-
-      return duration;
-    });
-
-    // last activation
-    this.contactSensorService.addCharacteristic(EveContactSensorLastActivation);
-    this.contactSensorService.getCharacteristic(EveContactSensorLastActivation).onGet(async () => {
-      if (this.loggingService.getInitialTime() === undefined) {
-        return 0;
-      } else if (this.contactSensorService.getCharacteristic(platform.Characteristic.ContactSensorState).value) {
-        return Math.round(new Date().valueOf() / 1000) - this.loggingService.getInitialTime();
-      } else {
-        let lastActivation = this.loggingService.history[this.loggingService.history.length - 1].time;
-        for (let i = this.loggingService.history.length - 1; i >= 0; --i) {
-          if (this.loggingService.history[i].status === false) {
-            lastActivation = this.loggingService.history[i].time;
-          } else {
-            break;
-          }
-        }
-        return lastActivation - this.loggingService.getInitialTime();
-      }
-    });
-
-    this.loggingService = new platform.fakeGatoHistoryService('door', this, { storage: 'fs', log: platform.log });
-
-    const dp = new Datapoint({
+    const datapoint = new Datapoint({
       ga: this.listen,
       dpt: 'DPT1.001',
       autoread: true,
     }, platform.connection);
 
-    dp.on('change', (oldValue: number, newValue: number) => {
-      this.contactSensorService.getCharacteristic(platform.Characteristic.ContactSensorState).updateValue(newValue);
-      this.loggingService._addEntry({ time: Math.round(new Date().valueOf() / 1000), status: newValue });
+    datapoint.on('change', (_oldValue: unknown, newValue: unknown) => {
+      this.updateContactState(newValue);
     });
   }
 
@@ -190,5 +82,180 @@ export class ContactSensorAccessory implements AccessoryPlugin {
       this.contactSensorService,
       this.loggingService,
     ];
+  }
+
+  private addEveCharacteristics(): void {
+    const EveContactSensorTimesOpened = this.createEveCharacteristic(
+      'Times Opened',
+      EVE_CONTACT_SENSOR_TIMES_OPENED_UUID,
+      this.platform.api.hap.Formats.UINT32,
+      undefined,
+      false,
+    );
+    const EveContactSensorOpenDuration = this.createEveCharacteristic(
+      'Open Duration',
+      EVE_CONTACT_SENSOR_OPEN_DURATION_UUID,
+      this.platform.api.hap.Formats.UINT32,
+      this.platform.api.hap.Units.SECONDS,
+      true,
+    );
+    const EveContactSensorClosedDuration = this.createEveCharacteristic(
+      'Closed Duration',
+      EVE_CONTACT_SENSOR_CLOSED_DURATION_UUID,
+      this.platform.api.hap.Formats.UINT32,
+      this.platform.api.hap.Units.SECONDS,
+      true,
+    );
+    const EveContactSensorLastActivation = this.createEveCharacteristic(
+      'Last Activation',
+      EVE_CONTACT_SENSOR_LAST_ACTIVATION_UUID,
+      this.platform.api.hap.Formats.UINT32,
+      this.platform.api.hap.Units.SECONDS,
+      false,
+    );
+
+    this.contactSensorService.addCharacteristic(EveContactSensorTimesOpened);
+    this.contactSensorService.getCharacteristic(EveContactSensorTimesOpened).onGet(async () => this.getTimesOpened());
+
+    this.contactSensorService.addCharacteristic(EveContactSensorOpenDuration);
+    this.contactSensorService.getCharacteristic(EveContactSensorOpenDuration).onGet(async () => this.getDuration(true));
+
+    this.contactSensorService.addCharacteristic(EveContactSensorClosedDuration);
+    this.contactSensorService.getCharacteristic(EveContactSensorClosedDuration).onGet(async () => this.getDuration(false));
+
+    this.contactSensorService.addCharacteristic(EveContactSensorLastActivation);
+    this.contactSensorService.getCharacteristic(EveContactSensorLastActivation).onGet(async () => this.getLastActivation());
+  }
+
+  private createEveCharacteristic(
+    displayName: string,
+    uuid: string,
+    format: string,
+    unit: string | undefined,
+    writable: boolean,
+  ): EveCharacteristicConstructor {
+    const platform = this.platform;
+    const permissions = [
+      platform.api.hap.Perms.PAIRED_READ,
+      platform.api.hap.Perms.NOTIFY,
+    ];
+
+    if (writable) {
+      permissions.push(platform.api.hap.Perms.PAIRED_WRITE);
+    }
+
+    return class EveContactSensorCharacteristic extends platform.Characteristic {
+      public static readonly UUID: string = uuid;
+
+      constructor() {
+        super(displayName, uuid, {
+          format,
+          unit,
+          perms: permissions,
+        });
+        this.value = this.getDefaultValue();
+      }
+    };
+  }
+
+  private updateContactState(value: unknown): void {
+    const isOpen = this.normalizeContactValue(value);
+    if (isOpen === undefined) {
+      this.platform.log.warn(`Ignoring unsupported KNX contact sensor value "${String(value)}" for "${this.name}".`);
+      return;
+    }
+
+    const contactState = isOpen
+      ? this.platform.Characteristic.ContactSensorState.CONTACT_NOT_DETECTED
+      : this.platform.Characteristic.ContactSensorState.CONTACT_DETECTED;
+
+    this.contactSensorService.getCharacteristic(this.platform.Characteristic.ContactSensorState).updateValue(contactState);
+    this.loggingService._addEntry({ time: Math.round(Date.now() / 1000), status: isOpen });
+  }
+
+  private normalizeContactValue(value: unknown): boolean | undefined {
+    if (value === true || value === 1) {
+      return true;
+    }
+
+    if (value === false || value === 0) {
+      return false;
+    }
+
+    if (typeof value === 'string') {
+      const normalized = value.trim().toLowerCase();
+      if (normalized === 'true' || normalized === '1' || normalized === 'open') {
+        return true;
+      }
+
+      if (normalized === 'false' || normalized === '0' || normalized === 'closed') {
+        return false;
+      }
+    }
+
+    return undefined;
+  }
+
+  private getTimesOpened(): CharacteristicValue {
+    let count = 0;
+    let previousStatus: boolean | undefined;
+
+    for (const entry of this.loggingService.history) {
+      if (entry.status === undefined) {
+        continue;
+      }
+
+      if (entry.status !== previousStatus && previousStatus !== true) {
+        count++;
+      }
+
+      previousStatus = entry.status;
+    }
+
+    return count;
+  }
+
+  private getDuration(open: boolean): CharacteristicValue {
+    let duration = 0;
+    let previousStatus: boolean | undefined;
+    let previousTime: number | undefined;
+
+    for (const entry of this.loggingService.history) {
+      if (entry.status === undefined || entry.time === undefined) {
+        continue;
+      }
+
+      if (previousStatus === open && previousTime !== undefined) {
+        duration += entry.time - previousTime;
+      }
+
+      previousStatus = entry.status;
+      previousTime = entry.time;
+    }
+
+    return duration;
+  }
+
+  private getLastActivation(): CharacteristicValue {
+    const initialTime = this.loggingService.getInitialTime();
+    if (initialTime === undefined || this.loggingService.history.length === 0) {
+      return 0;
+    }
+
+    if (this.contactSensorService.getCharacteristic(this.platform.Characteristic.ContactSensorState).value) {
+      return Math.round(Date.now() / 1000) - initialTime;
+    }
+
+    let lastActivation = this.loggingService.history[this.loggingService.history.length - 1].time ?? initialTime;
+    for (let i = this.loggingService.history.length - 1; i >= 0; --i) {
+      const historyTime = this.loggingService.history[i].time;
+      if (this.loggingService.history[i].status === false && historyTime !== undefined) {
+        lastActivation = historyTime;
+      } else {
+        break;
+      }
+    }
+
+    return lastActivation - initialTime;
   }
 }
--- src/platform.ts
+++ src/platform.ts
@@ -3,16 +3,24 @@ import type { API, StaticPlatformPlugin, Logging, PlatformConfig, AccessoryPlugi
 import fakegato from 'fakegato-history';
 import { Connection } from 'knx';
 
-import { ContactSensorAccessory } from './accessory.js';
+import { ContactSensorAccessory, type ContactSensorDeviceConfig } from './accessory.js';
 
+const DEFAULT_KNX_IP = '224.0.23.12';
+const DEFAULT_KNX_PORT = 3671;
+const KNX_GROUP_ADDRESS_PATTERN = /^[0-9]{1,4}\/[0-9]{1,4}\/[0-9]{1,4}$/;
+
+type NormalizedPlatformConfig = {
+  ip: string;
+  port: number;
+  devices: ContactSensorDeviceConfig[];
+};
 
 export class ContactSensorPlatform implements StaticPlatformPlugin {
   public readonly Service: typeof Service;
   public readonly Characteristic: typeof Characteristic;
   public readonly uuid: typeof uuid;
 
-  public readonly fakeGatoHistoryService;
-
+  public readonly fakeGatoHistoryService: unknown;
   public readonly connection: Connection;
 
   private readonly devices: ContactSensorAccessory[] = [];
@@ -27,32 +35,99 @@ export class ContactSensorPlatform implements StaticPlatformPlugin {
     this.uuid = api.hap.uuid;
     this.fakeGatoHistoryService = fakegato(this.api);
 
-    // connect
+    const normalizedConfig = this.normalizeConfig(config);
+
     this.connection = new Connection({
-      ipAddr: config.ip ?? '224.0.23.12',
-      ipPort: config.port ?? 3671,
+      ipAddr: normalizedConfig.ip,
+      ipPort: normalizedConfig.port,
       handlers: {
-        connected: function () {
+        connected: () => {
           log.info('KNX connected');
         },
-        error: function (connstatus: unknown) {
-          log.error(`KNX status: ${connstatus}`);
+        disconnected: () => {
+          log.info('KNX disconnected');
+        },
+        error: (connectionStatus: unknown) => {
+          log.error(`KNX status: ${String(connectionStatus)}`);
         },
       },
     });
 
-    // read devices
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    config.devices.forEach((element: any) => {
-      if (element.name !== undefined && element.listen) {
-        this.devices.push(new ContactSensorAccessory(this, element));
-      }
+    this.devices = normalizedConfig.devices.map((device) => new ContactSensorAccessory(this, device));
+
+    this.api.on('shutdown', () => {
+      this.connection.Disconnect();
     });
 
-    log.info('finished initializing!');
+    log.info(`Initialized ${this.devices.length} KNX contact sensor accessory/accessories`);
   }
 
   accessories(callback: (foundAccessories: AccessoryPlugin[]) => void): void {
     callback(this.devices);
+  }
+
+  private normalizeConfig(config: PlatformConfig): NormalizedPlatformConfig {
+    const ip = typeof config.ip === 'string' && config.ip.trim().length > 0
+      ? config.ip.trim()
+      : DEFAULT_KNX_IP;
+
+    const port = this.normalizePort(config.port);
+    const devices = this.normalizeDevices(config.devices);
+
+    return { ip, port, devices };
+  }
+
+  private normalizePort(value: unknown): number {
+    if (typeof value === 'number' && Number.isInteger(value) && value > 0 && value <= 65535) {
+      return value;
+    }
+
+    if (typeof value === 'string') {
+      const parsed = Number(value);
+      if (Number.isInteger(parsed) && parsed > 0 && parsed <= 65535) {
+        return parsed;
+      }
+    }
+
+    if (value !== undefined) {
+      this.log.warn(`Invalid KNX port "${String(value)}"; using ${DEFAULT_KNX_PORT}`);
+    }
+
+    return DEFAULT_KNX_PORT;
+  }
+
+  private normalizeDevices(value: unknown): ContactSensorDeviceConfig[] {
+    if (!Array.isArray(value)) {
+      this.log.error('No devices configured. Add at least one KNX contact sensor device.');
+      return [];
+    }
+
+    return value.flatMap((entry, index) => {
+      const device = this.normalizeDevice(entry, index);
+      return device === undefined ? [] : [device];
+    });
+  }
+
+  private normalizeDevice(value: unknown, index: number): ContactSensorDeviceConfig | undefined {
+    if (typeof value !== 'object' || value === null) {
+      this.log.warn(`Skipping device at index ${index}: expected an object.`);
+      return undefined;
+    }
+
+    const candidate = value as Record<string, unknown>;
+    const name = typeof candidate.name === 'string' ? candidate.name.trim() : '';
+    const listen = typeof candidate.listen === 'string' ? candidate.listen.trim() : '';
+
+    if (name.length === 0) {
+      this.log.warn(`Skipping device at index ${index}: missing name.`);
+      return undefined;
+    }
+
+    if (!KNX_GROUP_ADDRESS_PATTERN.test(listen)) {
+      this.log.warn(`Skipping device "${name}": invalid listen address "${listen}".`);
+      return undefined;
+    }
+
+    return { name, listen };
   }
 }
