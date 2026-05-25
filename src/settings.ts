0e5f16b7d2500609298cd06e80129667194256ba
Jendrik <jendrik@bertram.eu>
2026-05-25 12:51:50 +0200

    refactor: align plugin metadata constants

--- src/settings.ts
+++ src/settings.ts
@@ -1,16 +1,22 @@
 import { createRequire } from 'node:module';
 
+type PackageMetadata = {
+  displayName: string;
+  name: string;
+  version: string;
+};
+
 const require = createRequire(import.meta.url);
-const { displayName, version } = require('../package.json');
+const { displayName, name, version } = require('../package.json') as PackageMetadata;
 
 /**
- * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
+ * This is the name of the platform that users will use to register the plugin in the Homebridge config.json.
  */
 export const PLATFORM_NAME = 'knx-door';
 
 /**
- * This must match the name of your plugin as defined the package.json
+ * This must match the package name in package.json when Homebridge APIs require the plugin identifier.
  */
-export const PLUGIN_NAME = 'homebridge-' + PLATFORM_NAME;
+export const PLUGIN_NAME = name;
 export const PLUGIN_DISPLAY_NAME = displayName;
 export const PLUGIN_VERSION = version;
